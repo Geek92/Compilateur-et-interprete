4d24c652971cdd9b44f4d33daaba8123536337ac
Jourdain <>
2022-02-10 16:17:56 +0100

    déclaration des variable modifiés

--- compilateur/compil2.py
+++ compilateur/compil2.py
@@ -2,6 +2,23 @@ import json
 
 #compil_json("test.json")
 
+
+
+# /* g: globals[0] */
+#    globals[0] = iconst(1);
+
+# Var x = 5;
+# globals[0] = iconst(5);
+# Var y = 6;
+# globals[1] = iconst(6);
+
+
+# nombreVarGlobal = 0 # cette valeur sera toujours égale à len(tableValVar) donc pas besoin de créer cette variable
+
+tableVar = list()  # tableVar = ["x", "y"]
+tableValVar = list() # tableValVar = [5, 6]
+
+
 def compil_json(file):# fonction a appeller pour compiler un fichier json
         fichier = open(file)
         data = json.load(fichier)# on récupére un dictionnaire
@@ -17,31 +34,51 @@ def compil_json(file):# fonction a appeller pour compiler un fichier json
                     var = i["declarations"][0]
                     nomVar = var["id"]["name"]
                     valVar = var["init"]
-                    if type(valVar) == type(None):# la variable est déclarée mais non initialisée
-                        chaineAecrire = "word_u " + nomVar +";\n"
+                    if type(valVar) == type(None):# la variable est déclarée mais non initialisée # Var x;    
+                        #chaineAecrire = "word_u " + nomVar +";\n" # il faut changer cette ligne
+                        #globals[0] = iconst(1);
+                        chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(null);\n"
+                        tableVar.append(nomVar)
+                        tableValVar.append(valVar)
+                        #print(valVar)
                         fichier.write(chaineAecrire)
-                    else:# la variable est initialisée
-                        valVar = valVar["extra"]["raw"]
-                        valVar = str(valVar)#python ne veut pas que l'on concatène des chaines de caractères avec des entiers donc on caste en string
-                        chaineAecrire = "word_u " + nomVar + " = " + valVar +";\n"
-                        fichier.write(chaineAecrire)
+                    else:# la variable est initialisée                      
+                        if type("") == type(valVar["extra"]["rawValue"]):# si notre variable est une chaine de caractère
+                            valVar = valVar["extra"]["rawValue"]
+                            tableValVar.append(valVar)
+                            chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(\"" + valVar + "\");\n"
+                            tableVar.append(nomVar)                               
+                            fichier.write(chaineAecrire)                           
+                        else:# si la variable n'est pas une chaine de caractère
+                            valVar = valVar["extra"]["rawValue"]                          
+                            tableValVar.append(valVar)# on rajoute la valeur de la variable avant le nom de sa variable dans tableVar pour éviter que toutes les variables soit des chaines de caractères
+                            valVar = str(valVar)#python ne veut pas que l'on concatène des chaines de caractères avec des entiers donc on caste en string                           
+                            chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(" + valVar + ");\n"
+                            tableVar.append(nomVar)                               
+                            fichier.write(chaineAecrire)
+                        
                 else:#on déclare plusieurs variables sur la même ligne
                     for j in range(len(i["declarations"])):
                         var = i["declarations"][j]
                         nomVar = var["id"]["name"]
                         valVar = var["init"]
                         if type(valVar) == type(None):# la variable est déclarée mais non initialisée
-                            chaineAecrire = "word_u " + nomVar +";\n"
+                            chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(null);\n"
+                            tableVar.append(nomVar)
+                            tableValVar.append(valVar)
                             fichier.write(chaineAecrire)
                         else:# la variable est initialisée
                             if valVar["type"] == "NullLiteral":# la variable est initialisée à null
-                                valVar = "null"
-                                chaineAecrire = "word_u " + nomVar + " = " + valVar +";\n"
+                                chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(null);\n"
+                                tableVar.append(nomVar)
+                                tableValVar.append(None)
                                 fichier.write(chaineAecrire)
                             else:
                                 valVar = valVar["extra"]["raw"]
                                 valVar = str(valVar)#python ne veut pas que l'on concatène des chaines de caractères avec des entiers donc on caste en string
-                                chaineAecrire = "word_u " + nomVar + " = " + valVar +";\n"
+                                chaineAecrire = "globals[" + str(len(tableVar)) +"] = iconst(" + valVar + ");\n"
+                                tableVar.append(nomVar)
+                                tableValVar.append(valVar)
                                 fichier.write(chaineAecrire)
         fichier.write("return 0;\n ")#on a fini de lire le json, rajoutons ce qu'il faut pour terminer
         fichier.write("}")
